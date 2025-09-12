2ced85cf2d39136b320952656e94f025400ea27a
Sakthi-007 <sakthisiva2004@gmail.com>
2025-09-12 11:35:19 +0530

    backend connected successfully

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv';
 const app = express();
-
 import client from './database/dbconnection.js';
 dotenv.config();
 
-client.connect(function (err) {
-    if (err)
-        throw err;
-    client.query("SELECT VERSION()", [], function (err, result) {
-        if (err)
-            throw err;
 
-        console.log(result.rows[0].version);
-        // Do not end the client here, keep it open for app usage
-    });
-});
 
 const port = process.env.PORT;
 
