8fdbaad48f02595b58f628e92d53cbff4625e8ed
Rohit <ksrohit25@gmail.com>
2025-10-07 22:04:47 +0530

    style and import patch

--- client/src/pages/Transactions.jsx
+++ client/src/pages/Transactions.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import '../styles/transactions.css';
-import axios from 'axios';
+
 const Transactions = () => {
   // console.log(res)
   const [transactions,setTransactions] = useState([]);
